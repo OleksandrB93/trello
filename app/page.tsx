8262d4ddd116abf84c079b9347a57c59c9703f56
OleksandrB93 <barabashsan4ik@gmail.com>
2023-10-01 11:57:40 +0300

    create and fetch boards

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { BoardCard } from "@/components";
+import { prisma } from "@/prisma/db";
 
-export default function Home() {
+const Home = async () => {
+  const boards = await prisma.boards.findMany();
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 xl:gap-6">
-        <BoardCard id="1" title="Board1" />
-        <BoardCard id="2" title="Board2" />
+        {boards.map(({ title, id }) => (
+          <BoardCard key={id} title={title} id={id} />
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default Home;
